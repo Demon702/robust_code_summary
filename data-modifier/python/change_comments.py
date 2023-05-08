3dc95d60f315fe83392417b7a0370b78cb8014b5
alodha_umass_edu <alodha_umass_edu@gypsum-gpu055.unity.rc.umass.edu>
2023-05-08 01:31:14 +0000

    changed comments in python

--- data-modifier/python/change_comments.py
+++ data-modifier/python/change_comments.py
@@ -9,38 +9,51 @@ import os
 import re
 import ipdb
 
-input_path = "/work/pi_adrozdov_umass_edu/alodha_umass_edu/nlp_project/robust_code_summary/CodeT5/data/summarize/python/train.jsonl"
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input_path', type=str, default='../../CodeT5/data/summarize/python/valid.jsonl')
+parser.add_argument('-o', '--output_path', type=str, default='../../CodeT5/commented_code/summarize/python/valid.jsonl')
 
-output_path = "/work/pi_adrozdov_umass_edu/alodha_umass_edu/nlp_project/robust_code_summary/CodeT5/commented_code/summarize/python/train.jsonl"
+args = parser.parse_args()
+INPUT_PATH = args.input_path
+OUTPUT_PATH = args.output_path
 
-if not os.path.exists(os.path.dirname(output_path)):
-    os.makedirs(os.path.dirname(output_path))
+if not os.path.exists(os.path.dirname(OUTPUT_PATH)):
+    os.makedirs(os.path.dirname(OUTPUT_PATH))
 
-with open(input_path, 'r') as f:
+with open(INPUT_PATH, 'r') as f:
     data = [json.loads(line) for line in f.readlines()]
 
+f = open(OUTPUT_PATH, 'w+')
+
 for d in tqdm(data):
+    
     code = d['code']
     doc_string = d['docstring']
+    
     comment_pattern = r'""".+?"""'
     comments = re.findall(comment_pattern, code, re.DOTALL)
     
-    for comment in comments:
-        random_data = random.choice(data)
-        random_code = random_data['code']
-        random_comment_pattern = r'""".+?"""'
-        random_comments = re.findall(random_comment_pattern, random_code, re.DOTALL)
-        
-        if random_comments:
-            random_comment = random_comments[0]
-            code = code.replace(comment, random_comment)
+    random_data = random.choice(data)
+    random_code = random_data['code']
+    code_lines = random_code.split('\n')
+    commented_lines = ['#' + line for line in code_lines]
+    commented_code = '\n'.join(commented_lines)
+    
+
+    if comments:
+        comment = comments[0]
+        code = code.replace(comment, commented_code)
         
     code = code.replace(doc_string, '')
     code = re.sub(r"\"\"\"\s*\"\"\"", '', code)
     
-    with open(output_path, 'a') as f:
-        json.dump({'code': code, 'docstring': doc_string}, f)
-        f.write('\n')
-
+    # ipdb.set_trace()
         
-print("finished")
+    del d['code_tokens']
+    
+    d['code'] = code
+    json.dump(d, f)
+    f.write("\n")
+    
+f.close()
+    
